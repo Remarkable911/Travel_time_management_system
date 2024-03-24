d3c7c30a25d48c90d0e0413a2b0851f87b43b8d1
lsf <lisf2001@163.com>
2024-03-24 15:20:54 +0800

    3.24.2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pymysql
 from flask import Flask, render_template, request, redirect, flash
+import csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
@@ -40,15 +41,74 @@ def execute_query(sql, params=None, fetchone=False):
     # 返回查询结果
     return result
 
+# 导入数据order
+def import_csv_to_db1(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)  # 跳过标题行
+            
+            for row in csv_reader:
+                # 解析 CSV 文件中的每一行数据
+                orderid, ata, distance, simpleeta, driverid, sliceid, date = row
+                
+                # 构造插入数据的 SQL 语句
+                sql = "INSERT INTO `order` (orderid, ata, distance, simpleeta, driverid, sliceid, date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                # 执行 SQL 插入操作
+                execute_query(sql, (orderid, ata, distance, simpleeta, driverid, sliceid, date))
+        
+        flash('导入成功')
+    except Exception as e:
+        flash('导入失败，请检查文件格式')
+        print('Error:', e)
+# 导入数据weather
+def import_csv_to_db2(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)  # 跳过标题行
+            
+            for row in csv_reader:
+                # 解析 CSV 文件中的每一行数据
+                date,weather,hightemp,lowtemp = row
+                
+                # 构造插入数据的 SQL 语句
+                sql = "INSERT INTO `order` (date,weather,hightemp,lowtemp) VALUES (%s, %s, %s, %s)"
+                # 执行 SQL 插入操作
+                execute_query(sql, (date,weather,hightemp,lowtemp))
+        
+        flash('导入成功')
+    except Exception as e:
+        flash('导入失败，请检查文件格式')
+        print('Error:', e)
+# 导入数据intersection
+def import_csv_to_db3(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)  # 跳过标题行
+            
+            for row in csv_reader:
+                # 解析 CSV 文件中的每一行数据
+                crossid,crosstime,entranceid,exitid,orderid = row
+                
+                # 构造插入数据的 SQL 语句
+                sql = "INSERT INTO `order` (crossid,crosstime,entranceid,exitid,orderid) VALUES (%s, %s, %s, %s, %s)"
+                # 执行 SQL 插入操作
+                execute_query(sql, (crossid,crosstime,entranceid,exitid,orderid))
+        
+        flash('导入成功')
+    except Exception as e:
+        flash('导入失败，请检查文件格式')
+        print('Error:', e)
+
+
 # 主界面
 @app.route('/', methods=['GET', 'POST'])
-def menu():
-    if request.method == 'POST':
-        id = request.form.get('keyword')
-        sql = "SELECT * FROM `order` WHERE orderid=%s"
-        res = execute_query(sql, [id], fetchone=True)
-        print(res)
-    return render_template('menu.html')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    
+    return render_template('index.html')
 
 # 用户管理界面
 @app.route('/manage',methods=['GET', 'POST'])
@@ -78,7 +138,6 @@ def manage():
             email = request.form.get('email')
             role = request.form.get('role')
             status = request.form.get('state')
-            print(username, account, gender, phone, email, role, status)
             # 构造 SQL 插入语句
             sql = "INSERT INTO member (username, account, gender, phone, email, role, status) VALUES (%s, %s, %s, %s, %s, %s, %s)"
             data = (username, account, gender, phone, email, role, status)
@@ -109,9 +168,49 @@ def manage():
 # 数据处理界面
 @app.route('/processing', methods=['GET', 'POST'])
 def processing():
-    sql = "SELECT * FROM `order` ORDER BY orderid  LIMIT 100"
-    data = execute_query(sql)
-    return render_template('processing.html', data=data)
+    if request.method == 'POST':
+        # 获取按钮的名称或值
+        action = request.form.get('action')
+        if action == 'in-one':
+            
+            file = request.files['order_file']
+            # 保存上传的文件到服务器
+            file_path = 'uploads/' + file.filename
+            file.save(file_path)
+        
+            # 导入 CSV 文件到数据库
+            import_csv_to_db1(file_path)
+
+            return redirect('/processing')
+            
+        elif action == 'in-two':
+            file = request.files['weather_file']
+            # 保存上传的文件到服务器
+            file_path = 'uploads/' + file.filename
+            file.save(file_path)
+        
+            # 导入 CSV 文件到数据库
+            import_csv_to_db2(file_path)
+
+            return redirect('/processing')
+        elif action == 'in-three':
+            file = request.files['section_file']
+            # 保存上传的文件到服务器
+            file_path = 'uploads/' + file.filename
+            file.save(file_path)
+        
+            # 导入 CSV 文件到数据库
+            import_csv_to_db3(file_path)
+
+            return redirect('/processing')
+    else:
+        sql1 = "SELECT * FROM `order` ORDER BY create_at DESC LIMIT 10"
+        data1 = execute_query(sql1)
+        sql2 = "SELECT * FROM `weather` ORDER BY create_at DESC LIMIT 10"
+        data2 = execute_query(sql2)
+        sql3 = "SELECT * FROM `intersection` ORDER BY create_at DESC LIMIT 10"
+        data3 = execute_query(sql3)
+        return render_template('processing.html',data1=data1,data2=data2,data3=data3)
 
 # 登录界面
 @app.route('/login', methods=['GET', 'POST'])
